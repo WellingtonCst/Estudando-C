48b0ebbc07c9417f69c62c1a5fb543a9698d24e5
Wellington Costa <costawellington702@gmail.com>
2024-01-17 17:44:31 -0300

    soma

---
+++ exercicios-basicos/exercicio-dia-17-01-2024/soma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num1,num2;
+    int resultado;
+
+    printf("\nDigite um valor: ");
+    scanf("%d", &num1);
+
+    printf("\nDigite outro valor: ");
+    scanf("%d", &num2);
+
+    resultado = num1+num2;
+
+    printf("A Soma dos valores e igual a: %d", resultado);
+    
+}
